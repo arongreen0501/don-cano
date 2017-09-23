634df09f6bed58ecb7eed65859c01dd95ef3f007
Aron Greenj <arongreen0501@gmail.com>
2017-09-23 14:10:54 +0000

    modifying stuff

--- themes/dc/footer.php
+++ themes/dc/footer.php
@@ -13,23 +13,31 @@
 
 	</div><!-- #content -->
 
-	<footer class="site-footer">
+	<footer id="footer">
 		<div class="container">
+
 			<?php 
 
 				$image = get_field('logo_gray', 'option');
 
 				if( !empty($image) ): ?>
-
-					<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+					<div>
+						<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+					</div>
 
 			<?php endif; ?>
-			
-			<p><?php the_field('general_site_title', 'option'); ?></p>
-			<p><?php the_field('info_street_address', 'option'); ?></p>
-			<p><?php the_field('info_location', 'option'); ?></p>
-			<p><?php the_field('info_phone_number', 'option'); ?></p>
-			<p><?php the_field('info_mail', 'option'); ?></p>
+
+			<div class="footer_content">
+				<p class="footer_title"><?php the_field('general_site_title', 'option'); ?></p>
+				<div class="footer_info">
+					<p><?php the_field('info_street_address', 'option'); ?></p>
+					<p><?php the_field('info_location', 'option'); ?></p>
+				</div>
+				<div class="footer_info">
+					<p><?php the_field('info_phone_number', 'option'); ?></p>
+					<p><?php the_field('info_mail', 'option'); ?></p>
+				</div>
+			</div>
 		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- themes/dc/page-history.php
+++ themes/dc/page-history.php
@@ -5,15 +5,23 @@
 ?>
 <?php get_header(); ?>
 
-	<section id="history">
+	<?php
+		$hero_image = get_field( 'hero_image' );
+		$size = 'hero-size';
+		$thumb = $image[ 'sizes' ][ $size ];
+	?>
 
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<section class="hero align-items-center align-items-md-end" style="background-image: url(<?php echo $hero_image['url']; ?>)">
+		<div class="container-fluid">
 
-				<h1><?php the_title(); ?></h1>
-                <?php the_content(); ?>
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-			<?php endwhile; endif; ?>
+			<h1><?php the_field('hero_title'); ?></h1>
+			<h2><?php the_field('hero_subtitle'); ?></h2>
+		
+		<?php endwhile; endif; ?>
 
-	</section><!-- #primary -->
+		</div>
+	</section>
     
 <?php get_footer(); ?>
